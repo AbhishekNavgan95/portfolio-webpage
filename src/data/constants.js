27bbe80ac205760739e790be243df836a809179e
AbhishekNavgan95 <navganabhishek90@gmail.com>
2024-08-18 23:25:21 +0530

    updated skills

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,17 +7,17 @@ export const url = {
 
 export const skills = [
   {
+    URL: "https://github.com/marwin1991/profile-technology-icons/assets/136815194/5f8c622c-c217-4649-b0a9-7e0ee24bd704",
+  }, // Next.js
+  {
+    URL: "https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png",
+  }, // React
+  {
     URL: "https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png",
   }, // JavaScript
   {
     URL: "https://user-images.githubusercontent.com/25181517/183890598-19a0ac2d-e88a-4005-a8df-1ee36782fde1.png",
   }, // TypeScript
-  {
-    URL: "https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png",
-  }, // React
-  {
-    URL: "https://user-images.githubusercontent.com/25181517/187896150-cc1dcb12-d490-445c-8e4d-1275cd2388d6.png",
-  }, // Redux
   {
     URL: "https://user-images.githubusercontent.com/25181517/183568594-85e280a7-0d7e-4d1a-9028-c8c2209e073c.png",
   }, // Node.js
@@ -25,11 +25,11 @@ export const skills = [
     URL: "https://user-images.githubusercontent.com/25181517/183859966-a3462d8d-1bc7-4880-b353-e2cbed900ed6.png",
   }, // Express
   {
+    URL: "https://user-images.githubusercontent.com/25181517/187896150-cc1dcb12-d490-445c-8e4d-1275cd2388d6.png",
+  }, // Redux
+  {
     URL: "https://user-images.githubusercontent.com/25181517/182884177-d48a8579-2cd0-447a-b9a6-ffc7cb02560e.png",
   }, // mongoDB
-  {
-    URL: "https://github.com/marwin1991/profile-technology-icons/assets/136815194/5f8c622c-c217-4649-b0a9-7e0ee24bd704",
-  }, // Next.js
   {
     URL: "https://user-images.githubusercontent.com/25181517/202896760-337261ed-ee92-4979-84c4-d4b829c7355d.png",
   }, // Tailwind CSS
@@ -45,12 +45,12 @@ export const skills = [
   {
     URL: "https://user-images.githubusercontent.com/25181517/183898674-75a4a1b1-f960-4ea9-abcb-637170a00a75.png",
   }, // CSS
-  {
-    URL: "https://user-images.githubusercontent.com/25181517/187070862-03888f18-2e63-4332-95fb-3ba4f2708e59.png",
-  }, // websocket
-  {
-    URL: "https://user-images.githubusercontent.com/25181517/192107856-aa92c8b1-b615-47c3-9141-ed0d29a90239.png",
-  }, // GraphQL
+  // {
+  //   URL: "https://user-images.githubusercontent.com/25181517/187070862-03888f18-2e63-4332-95fb-3ba4f2708e59.png",
+  // }, // websocket
+  // {
+  //   URL: "https://user-images.githubusercontent.com/25181517/192107856-aa92c8b1-b615-47c3-9141-ed0d29a90239.png",
+  // }, // GraphQL
   {
     URL: "https://user-images.githubusercontent.com/25181517/117201156-9a724800-adec-11eb-9a9d-3cd0f67da4bc.png",
   }, // Java
@@ -66,7 +66,7 @@ export const skills = [
   {
     URL: "https://user-images.githubusercontent.com/25181517/192108372-f71d70ac-7ae6-4c0d-8395-51d8870c2ef0.png",
   }, // Git
-  {
-    URL: "https://user-images.githubusercontent.com/25181517/192108374-8da61ba1-99ec-41d7-80b8-fb2f7c0a4948.png",
-  }, // GitHub
+  // {
+  //   URL: "https://user-images.githubusercontent.com/25181517/192108374-8da61ba1-99ec-41d7-80b8-fb2f7c0a4948.png",
+  // }, // GitHub
 ];
